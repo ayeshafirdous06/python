276f436544e364fcc613783afd696c93ad6ecce1
Ayesha firdous <ak9307602@gmail.com>
2026-09-07 15:35:48 +0530

    Add files via upload

---
+++ practice/python/operators/arithmetic/sum.diff,product,quotient,remainder.py
@@ -0,0 +1,16 @@
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+
+print("sum of a and b is: ",a+b)
+
+print("difference of a and b is:", a-b)
+
+print("product of a and b is:", a*b)
+
+print("division of a and b is:", a/b)
+
+print("float division of a and b is:", a//b)
+
+print("exponent of a and b is:", a**b)
+
+print("quotient of a and b is:", a%b)
